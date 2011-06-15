a55efaad3b3f366ac7fc0a2ba4fd349b640a2b7a
Dominic Yu <gobbledegook@users.noreply.github.com>
2011-06-15 06:03:07 +0000

    r82: fixed deployment script dirs

--- rootcanal/deployment/README.txt
+++ rootcanal/deployment/README.txt
@@ -1,7 +1,7 @@
 To update to the latest revision, do the following:
 
-svn update rootcanal
-rootcanal/deployment/make.sh
+svn update svn-rootcanal
+svn-rootcanal/deployment/make.sh
 
 This copies over the files to the appropriate places
 and minifies the javascript files.
--- rootcanal/deployment/make.sh
+++ rootcanal/deployment/make.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 # use -rtvni to do a dry run with itemized changes
 
-rsync -rtv --exclude '.svn' --exclude 'js' ~/rootcanal/web/ ~/public_html
-rsync -rtv --exclude '.svn' ~/rootcanal/perl/STEDT/ ~/pm/STEDT
+rsync -rtv --exclude '.svn' --exclude 'js' ~/svn-rootcanal/web/ ~/public_html
+rsync -rtv --exclude '.svn' ~/svn-rootcanal/perl/STEDT/ ~/pm/STEDT
 # add new files to js directory if necessary, but let minify do the replacing
-rsync -rtv --exclude '.svn' --ignore-existing --delete ~/rootcanal/web/js/ ~/public_html/js
-find ~/rootcanal/web/js/ -name "*.js" -exec perl ~/rootcanal/deployment/minify.pl {} \;
+rsync -rtv --exclude '.svn' --ignore-existing --delete ~/svn-rootcanal/web/js/ ~/public_html/js
+find ~/svn-rootcanal/web/js/ -name "*.js" -exec perl ~/svn-rootcanal/deployment/minify.pl {} \;
