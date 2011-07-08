7cd7d43b0db3e25cca3b1198699db98aad15607a
Dominic Yu <gobbledegook@users.noreply.github.com>
2011-07-08 09:07:55 +0000

    r159: deploy script now bundles in an svn update command and logs the current deployed revision in ~/deployed.txt

--- rootcanal/deployment/make.sh
+++ rootcanal/deployment/make.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 # use -rtvni to do a dry run with itemized changes
 
+svn up ~/svn-rootcanal
 rsync -rtv --exclude '.svn' --exclude 'js' ~/svn-rootcanal/web/ ~/public_html
 rsync -rtv --exclude '.svn' ~/svn-rootcanal/perl/STEDT/ ~/pm/STEDT
 # add new files to js directory if necessary, but let minify do the replacing
 rsync -rtv --exclude '.svn' --ignore-existing --delete ~/svn-rootcanal/web/js/ ~/public_html/js
 find ~/svn-rootcanal/web/js/ -name "*.js" -exec perl ~/svn-rootcanal/deployment/minify.pl {} \;
+
+svn info ~/svn-rootcanal | grep 'Revision' > ~/deployed.txt
+
