ab5fdc0b6f0a9eb0d6600b6228b5955f72f0d544
Dominic Yu <gobbledegook@users.noreply.github.com>
2011-07-14 23:03:43 +0000

    r180: multiple notes on lexicon recs

--- rootcanal/web/js/etymon.js
+++ rootcanal/web/js/etymon.js
@@ -4,10 +4,12 @@ setup['lexicon']['notes.rn'] = {
 	noedit: true,
 	size: 80,
 	transform : function (v) {
-		if (v === '0') return (stedtuserprivs & 1) ? '<a href="#" class="lexadd">[+]</a>' : '';
+		var addlink = '<a href="#" class="lexadd">[+]</a>';
+		if (v === '0') return (stedtuserprivs & 1) ? addlink : '';
 		var a = $A(v.match(/\d+/g)).map(function (s) {
 			return '<a href="#foot' + s + '" id="toof' + s + '">' + s + '</a>';
 		});
+		a.push(addlink);
 		return a.join(' ');
 	}
 };
