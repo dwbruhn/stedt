38faafdf6c2a34e3d91c07aeeb4f07bacf49ddea
Daniel Bruhn <dwbruhn@users.noreply.github.com>
2011-07-15 00:03:08 +0000

    r183: Srcabbr link in edit/lexicon now opens in a new window.

--- rootcanal/web/js/tbl/lexicon.js
+++ rootcanal/web/js/tbl/lexicon.js
@@ -1,5 +1,5 @@
 setup['lexicon']['lexicon.srcid'].transform = function (v, k, rec, n) {
-	return '<a href="' + baseRef + 'edit/srcbib?srcbib.srcabbr=' + rec[n-1] + '">' + rec[n-1] + '</a>'
+	return '<a href="' + baseRef + 'edit/srcbib?srcbib.srcabbr=' + rec[n-1] + '" target="edit_src">' + rec[n-1] + '</a>'
 		+ (v ? ':&thinsp;' + v : '');
 };
 
