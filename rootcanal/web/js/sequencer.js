8763f237206aeade46e51be88f5895fa712c9515
Dominic Yu <gobbledegook@users.noreply.github.com>
2011-07-30 11:22:48 +0000

    r206: some javascript code refactoring

--- rootcanal/web/js/sequencer.js
+++ rootcanal/web/js/sequencer.js
@@ -1,5 +1,40 @@
-// re-letter all the "etymon" divs, given a container
-var reletter = function (c) {
+var SEQ = Class.create({
+initialize: function () {	
+	$('all_tags').on('change', '.paf-btn', function (e) {this.repaf(e.findElement())}.bind(this));
+	this.init_sort_all();
+},
+
+init_sort_all: function () {
+	var all_c = $$(".etymon-container,.uncontainer");
+	all_c.each(function (x) {this.init_sort(x, all_c)}.bind(this));
+	Sortable.create('all_tags', {
+		tag:'div', only:'allofam', scroll:window,
+		onUpdate:this.reseq.bind(this)
+	});
+},
+
+// init a single container for sorting. optionally pass in the list of the other containers for efficiency.
+init_sort: function (c,cc) {
+	if (!cc) { cc = $$(".etymon-container,.uncontainer") }
+	Sortable.create(c.id, {
+		tag:'div', only:'etymon', scroll:window,
+		containment:cc,
+		dropOnEmpty:true,
+		onUpdate:this.handle_etymon_move.bind(this)
+	});
+},
+
+// cleanup method to be called once after handle_etymon_move
+cleanup : function () {
+	this.reseq();
+	if (this.cleanup_new) {
+		this.init_sort_all(); // call this after reseq() since reseq might change some classes
+	}
+	this.cleanup_scheduled = false;
+},
+
+// re-letter all the "etymon" divs, given a container; also re-init_sort
+reletter: function (c) {
 	if (c.up('.allofam-noseq')) {
 		$$('#' + c.id + ' .etymon').each(function (e) {
 			e.down('.seq-str').innerHTML = '0';
@@ -21,17 +56,17 @@ var reletter = function (c) {
 		c.down('.paf-btn').checked = false;
 		c.down('.paf-btn').disabled = true;
 	}
-};
+},
 
-var reseq = function () {
+reseq: function () {
 	$$('.allofam').each(function (a,i) {
 		a.down('.seq-num').innerHTML = i+1;
-		reletter(a.down('.etymon-container'));
-	});
-};
+		this.reletter(a.down('.etymon-container'));
+	}.bind(this));
+},
 
-var handle_etymon_move = function (c) {
-	var etymon, a, old_a, all_containers;
+handle_etymon_move: function (c) {
+	var etymon, a, old_a, do_cleanup = false;
 	if (c.hasClassName('uncontainer')) { // if 'fake' container, make a new one and reseq
 		old_a = c.up('.allofam'); // may be null if it's actually .allofam-noseq
 		etymon = c.down('.etymon');
@@ -40,58 +75,46 @@ var handle_etymon_move = function (c) {
 		a = $('all_tags').firstDescendant().clone(true);
 		a.id = 'fam_' + (++last_fam_seq);
 		a.down('.uncontainer').id = 'deallofam_' + last_fam_seq;
-		a.down('.uncontainer').innerHTML = ''; // clear this out since it might still have a copy of the moved item
+		a.down('.uncontainer').update(); // clear this out since it might still have a copy of the moved item
 
+		// put the etymon in it
 		c = a.down('.etymon-container');
 		c.id = 'allofams_' + last_fam_seq;
-		c.innerHTML = '';
-		c.insert(etymon);
+		c.update().insert(etymon);
 
+		// put it in the right place
 		if (old_a) {
 			old_a.insert({after:a});
 			if (!old_a.down('.etymon-container').firstChild) { old_a.remove(); } // see note under "else", below
-		} else {
-			$('all_tags').insert({top:a}); // special case for inserting at the top of the list
-		}
-		reseq();
-		
-		// re-init sorting for everything since we added a new container
-		all_containers = $$(".etymon-container,.uncontainer");
-		all_containers.each(function (x) {
-			Sortable.create(x.id, {tag:'div', only:'etymon', scroll:window,
-				containment:all_containers,
-				dropOnEmpty:true,
-				onUpdate:handle_etymon_move
-			});
-		});
-		Sortable.create('all_tags', {tag:'div', only:'allofam', scroll:window, onUpdate:reseq});
+		} else { $('all_tags').insert({top:a}) }
+		this.cleanup_new = true;
+		do_cleanup = true;
 	} else if (!c.firstChild) { // if empty container, delete it and reseq
 		// but make sure that (1) it's not .allofam-noseq, and
 		// (2) it really is empty (i.e., prevent the case where you drag the last etymon into the fake container below it and the whole thing gets deleted!
 		// easier to handle it here, delaying the remove() call until later [see above], than to prevent the user from dragging it there in the first place.)
 		if (c.up('.allofam') && !c.up('.allofam').down('.uncontainer').firstChild) {
 			c.up('.allofam').remove();
-			reseq();
+			do_cleanup = true;
 		}
 	} else {
-		reletter(c);
-		Sortable.create(c.id, {tag:'div', only:'etymon', scroll:window,
-			containment:$$(".etymon-container,.uncontainer"),
-			dropOnEmpty:true,
-			onUpdate:handle_etymon_move
-		});
+		this.reletter(c);
+		this.init_sort(c);
 	}
-};
+	if (do_cleanup && !this.cleanup_scheduled) {
+		this.cleanup.bind(this).defer();
+		this.cleanup_scheduled = true;
+	}
+},
 
 // checkbox for PAF
 // clicking on a PAF btn to "on" automatically turns off the other one(s)
 // the other one(s) get shifted down and everything gets re-lettered
 // move new one to top
-var repaf = function (x) {
-	var container = x.up('.etymon-container');
+repaf: function (x) {
+	var c = x.up('.etymon-container');
 	var was_paf = x.parentNode.className === 'paf';
-	var etyma = $$('#' + container.id + ' .paf');
-	etyma.each(function (e,i) {
+	$$('#' + c.id + ' .paf').each(function (e) {
 		e.down('.paf-btn').checked = false;
 		e.className = 'etymon';
 	});
@@ -100,47 +123,26 @@ var repaf = function (x) {
 		x = x.up('.etymon');
 		x.className = 'paf';
 		x.down('.seq-str').innerHTML = x.up('.allofam').down('.seq-num').innerHTML;
-		container.insert({top:x});
+		c.insert({top:x});
 	}
-	reletter(container);
-	// re-init the sorting for this container only
-	Sortable.create(container.id, {tag:'div', only:'etymon', scroll:window,
-		containment:$$(".etymon-container,.uncontainer"),
-		dropOnEmpty:true,
-		onUpdate:handle_etymon_move
-	}); // PAFs can't be sorted
-};
+	this.reletter(c);
+	this.init_sort(c); // PAFs can't be sorted
+},
+});
 
-// send list of lists, starting from 0
-// each item is list of tag nums
-// first item is 'P' if first item is PAF
+new SEQ();
+
+// send list of lists of tag nums (starting with the "n/a" set).
+// first item of tagnum list is 'P' if first tagnum is PAF.
 var self_serialize = function () {
 	if (!confirm("Are you sure you want to save the current sequencing?")) return false;
 	var list = $$('.allofam-noseq, .allofam').map(function (a) {
-		var list = Selector.findChildElements(a, ['.etymon','.paf']).map(function (e) {
+		var tags = Selector.findChildElements(a, ['.etymon','.paf']).map(function (e) {
 			return e.id.sub(/\D+/,'');
 		});
-		if (a.down('.paf')) { list.unshift('P'); }
-		return list;
+		if (a.down('.paf')) { tags.unshift('P'); }
+		return tags;
 	});
 	$('seqs_input').setValue(Object.toJSON(list));
 	return true;
 };
-
-
-// initialization stuff
-$$('.paf-btn').each(function (x) {
-	x.observe('change', function (e) {
-		repaf(e.findElement('input'));
-		e.stop();
-	});
-});
-var all_containers = $$(".etymon-container,.uncontainer");
-all_containers.each(function (x) {
-	Sortable.create(x.id, {tag:'div', only:'etymon', scroll:window,
-		containment:all_containers,
-		dropOnEmpty:true,
-		onUpdate:handle_etymon_move
-	});
-});
-Sortable.create('all_tags', {tag:'div', only:'allofam', scroll:window, onUpdate:reseq});
