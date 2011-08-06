82565aee03879c30e9322cdb1ef9edfbefd883df
Dominic Yu <gobbledegook@users.noreply.github.com>
2011-08-06 20:51:48 +0000

    r218: switch to --itemize-changes mode in deployment script switch to google's closure compiler for javascript minification

--- rootcanal/deployment/make.sh
+++ rootcanal/deployment/make.sh
@@ -2,10 +2,10 @@
 # use -rtvni to do a dry run with itemized changes
 
 svn up ~/svn-rootcanal
-rsync -rtv --exclude '.svn' --exclude 'js' ~/svn-rootcanal/web/ ~/public_html
-rsync -rtv --exclude '.svn' ~/svn-rootcanal/perl/STEDT/ ~/pm/STEDT
+rsync -rti --exclude '.svn' --exclude 'js' ~/svn-rootcanal/web/ ~/public_html
+rsync -rti --exclude '.svn' ~/svn-rootcanal/perl/STEDT/ ~/pm/STEDT
 # add new files to js directory if necessary, but let minify do the replacing
-rsync -rtv --exclude '.svn' --ignore-existing --delete ~/svn-rootcanal/web/js/ ~/public_html/js
+rsync -rti --exclude '.svn' --ignore-existing --delete ~/svn-rootcanal/web/js/ ~/public_html/js
 find ~/svn-rootcanal/web/js/ -name "*.js" -exec perl ~/svn-rootcanal/deployment/minify.pl {} \;
 
 svn info ~/svn-rootcanal | grep 'Revision' > ~/deployed.txt
--- rootcanal/deployment/minify.pl
+++ rootcanal/deployment/minify.pl
@@ -3,20 +3,38 @@
 # by Dominic Yu
 # read in a javascript file and output the minified version in the right dir
 
-use JavaScript::Packer;
-undef $/; # 'slurp' mode to read in a whole file
-
 my $in = shift @ARGV;
 my $out = $in;
 $out =~ s|^.*?/js/|$ENV{HOME}/public_html/js/|;
 # print "$in -> $out\n";
 # exit;
+
+my $google_compiler = 1;
+if ($google_compiler) {
+	# it's slow, so skip if possible
+	my ($last_rev) = `cat $ENV{HOME}/deployed.txt` =~ /(\d+)$/g;
+	my ($last_mod) = `svn info $in | grep 'Last Changed Rev'` =~ /(\d+)$/g;
+	if ($last_mod <= $last_rev) {
+#		print "$in is at $last_mod, not newer than $last_rev\n";
+		exit;
+	}
+} else {
+	require JavaScript::Packer;
+	import JavaScript::Packer;
+}
+
+undef $/; # 'slurp' mode to read in a whole file
 open F, "<$in" or die $!;
 open G, "<$out" or die $!;
-my $minified = JavaScript::Packer::minify(\<F>, {remove_copyright=>1});
+if ($google_compiler) {
+	print STDERR "analyzing $out... ";
+}
+my $minified = $google_compiler
+	? `java -jar $ENV{HOME}/lib/bin/compiler.jar --js $in`
+	: JavaScript::Packer::minify(\<F>, {remove_copyright=>1});
 my $dst_txt = <G>;
 if ($dst_txt eq $minified) {
-	# print STDERR "skipped  $out\n";
+	print STDERR "skipped.\n" if $google_compiler;
 	exit;
 }
 close G or die $!;
