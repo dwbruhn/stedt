9af361c5f50f73ea37f7b1dee917af34d6123cbb
Dominic Yu <gobbledegook@users.noreply.github.com>
2011-10-26 20:24:42 +0000

    r269: let authorizers edit chapter notes etyma -> lexicon opens in new window

--- rootcanal/web/js/tbl/etyma.js
+++ rootcanal/web/js/tbl/etyma.js
@@ -1,5 +1,5 @@
 setup['etyma']['num_recs'].transform = function (v,key) {
 	 return v != 0
-		? '<a href="' + baseRef + 'edit/lexicon?analysis=' + key + '">' + v + '&nbsp;r\'s</a>'
+		? '<a href="' + baseRef + 'edit/lexicon?analysis=' + key + '" target="stedt_lexicon">' + v + '&nbsp;r\'s</a>'
 		: v + '&nbsp;r\'s';
 };
