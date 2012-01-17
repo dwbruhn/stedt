bd83b8b632a878aeaf4588ec9d68c35a744d865e
stedtnik <stedt2ec48@berkeley.fake>
2012-01-17 04:35:31 +0000

    r346: oops. need this version of the script...

--- rootcanal/sql/mkm.sh
+++ rootcanal/sql/mkm.sh
@@ -1,5 +1,5 @@
 #
-mysql -D stedt -e "SELECT rn,reflex,gloss,gfn,language,grp,grpno,languagenames.srcabbr,lexicon.srcid,semcat,lexicon.lgid FROM lexicon,languagenames,languagegroups WHERE lexicon.lgid=languagenames.lgid AND languagenames.grpid=languagegroups.grpid;" > lexicon.csv
+mysql -D stedt -e "SELECT rn,reflex,gloss,gfn,language,grp,grpno,languagenames.srcabbr,lexicon.srcid,semkey,lexicon.lgid FROM lexicon,languagenames,languagegroups WHERE lexicon.lgid=languagenames.lgid AND languagenames.grpid=languagegroups.grpid;" > lexicon.csv
 #
 #mysql -D stedt -e "SELECT rn,analysis,reflex,gloss,semcat,lgid from lexicon;" > lexicon.csv &
 perl morphemes.pl < lexicon.csv > morphs.csv 
