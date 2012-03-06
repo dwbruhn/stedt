9349e50f1aa23aa524956c77cc95fc4a815a3264
stedtnik <stedt2ec48@berkeley.fake>
2012-03-06 21:59:43 +0000

    r439: this version needs uid and password as command line args.

--- rootcanal/sql/mkm.sh
+++ rootcanal/sql/mkm.sh
@@ -1,10 +1,8 @@
 #
-mysql -D stedt -e "SELECT rn,reflex,gloss,gfn,language,grp,grpno,languagenames.srcabbr,lexicon.srcid,semkey,lexicon.lgid FROM lexicon,languagenames,languagegroups WHERE lexicon.lgid=languagenames.lgid AND languagenames.grpid=languagegroups.grpid;" > lexicon.csv
-#
-#mysql -D stedt -e "SELECT rn,analysis,reflex,gloss,semcat,lgid from lexicon;" > lexicon.csv &
+mysql -D stedt --default-character-set=utf8 -u $1 -p$2 -e "SELECT rn,reflex,gloss,gfn,language,grp,grpno,languagenames.srcabbr,lexicon.srcid,semkey,lexicon.lgid FROM lexicon,languagenames,languagegroups WHERE lexicon.lgid=languagenames.lgid AND languagenames.grpid=languagegroups.grpid;" > lexicon.csv
 perl morphemes.pl < lexicon.csv > morphs.csv 
 perl transduce2.pl < morphs.csv > morphemes.csv
-mysql --local stedt < ct.sql 
+mysql --default-character-set=utf8 --local stedt -u $1 -p$2 < ct.sql 
 mv morphemes.csv morphemes.txt
-mysqlimport --local stedt morphemes.txt
-mysql --local stedt < mkindex.sql
+mysqlimport --local --default-character-set=utf8 -u $1 -p$2 stedt  morphemes.txt
+mysql --local stedt --default-character-set=utf8 -u $1 -p$2 < mkindex.sql
