f94f731b5c79de830fe1b5ebaf0ddf2468ad2563
stedtnik <stedt2ec48@berkeley.fake>
2012-04-16 21:20:01 +0000

    r473: create a table to hold "support" for soundlaws (i.e. one row per sound law per reflex)

--- rootcanal/sql/mkSoundLaws.sql
+++ rootcanal/sql/mkSoundLaws.sql
@@ -1,7 +1,8 @@
-DROP TABLE IF EXISTS sl;
-CREATE TABLE sl 
+DROP TABLE IF EXISTS soundlawsupport;
+CREATE TABLE soundlawsupport 
 SELECT 
 x.rn AS rn,
+x.rn AS slid,
 x.tag AS tag,
 m.mseq AS mseq,
 x.ind AS ind,
@@ -18,9 +19,10 @@ m.morpheme AS reflex
 FROM lx_et_hash x JOIN morphemes m on (x.rn=m.rn) JOIN etyma e WHERE e.tag = x.tag and m.mseq=x.ind
 ;
 
-INSERT INTO sl 
+INSERT INTO soundlawsupport 
 SELECT 
 x.rn AS rn,
+x.rn AS slid,
 x.tag AS tag,
 x.ind AS ind,
 m.mseq AS mseq,
@@ -37,17 +39,17 @@ m.morpheme AS reflex
 FROM lx_et_hash x JOIN morphemes m on (x.rn=m.rn) JOIN etyma e WHERE e.tag = x.tag and m.mseq=x.ind
 ;
 
-update sl set ancestor = left(ancestor,instr(ancestor,'⪤')-1) WHERE instr(ancestor,'⪤') > 0;
-update sl set ancestor = left(ancestor,instr(ancestor,' ')-1) WHERE instr(ancestor,' ') > 0;
-update sl set ancestor = left(ancestor,instr(ancestor,'=')-1) WHERE instr(ancestor,'=') > 0;
-update sl set ancestor = left(ancestor,instr(ancestor,'~')-1) WHERE instr(ancestor,'~') > 0;
-update sl set outcome = ltrim(outcome), ancestor=ltrim(ancestor);
-update sl set outcome = rtrim(outcome), ancestor=rtrim(ancestor);
+update soundlawsupport set ancestor = left(ancestor,instr(ancestor,'⪤')-1) WHERE instr(ancestor,'⪤') > 0;
+update soundlawsupport set ancestor = left(ancestor,instr(ancestor,' ')-1) WHERE instr(ancestor,' ') > 0;
+update soundlawsupport set ancestor = left(ancestor,instr(ancestor,'=')-1) WHERE instr(ancestor,'=') > 0;
+update soundlawsupport set ancestor = left(ancestor,instr(ancestor,'~')-1) WHERE instr(ancestor,'~') > 0;
+update soundlawsupport set outcome = ltrim(outcome), ancestor=ltrim(ancestor);
+update soundlawsupport set outcome = rtrim(outcome), ancestor=rtrim(ancestor);
 
 DROP TABLE IF EXISTS soundlaws;
-CREATE TABLE soundlaws SELECT rn,tag AS corrid, slot,protolg,ancestor,outcome,language,'' AS context,count(*) AS n,srcabbr,srcid,lgid FROM sl  group by slot,ancestor,outcome,language ORDER BY n;
+CREATE TABLE soundlaws SELECT rn,tag AS corrid, slot,protolg,ancestor,outcome,language,'' AS context,count(*) AS n,srcabbr,srcid,lgid FROM soundlawsupport group by slot,ancestor,outcome,language ORDER BY n;
 ALTER TABLE soundlaws ADD id INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (id);
 DELETE FROM soundlaws WHERE n < 2;
 DELETE FROM soundlaws WHERE outcome = '' OR ancestor = '';
 
-DROP TABLE sl;
+UPDATE soundlawsupport l SET slid = (SELECT s.id FROM soundlaws s WHERE s.slot=l.slot AND s.ancestor=l.ancestor AND s.outcome=l.outcome AND s.language=l.language);
