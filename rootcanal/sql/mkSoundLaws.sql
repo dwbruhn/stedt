74c157f1e319d788500dd370906451975052227d
stedtnik <stedt2ec48@berkeley.fake>
2012-04-17 03:30:43 +0000

    r474: move indexing commands into this file. (a modest simplication)

--- rootcanal/sql/mkSoundLaws.sql
+++ rootcanal/sql/mkSoundLaws.sql
@@ -47,9 +47,30 @@ update soundlawsupport set outcome = ltrim(outcome), ancestor=ltrim(ancestor);
 update soundlawsupport set outcome = rtrim(outcome), ancestor=rtrim(ancestor);
 
 DROP TABLE IF EXISTS soundlaws;
-CREATE TABLE soundlaws SELECT rn,tag AS corrid, slot,protolg,ancestor,outcome,language,'' AS context,count(*) AS n,srcabbr,srcid,lgid FROM soundlawsupport group by slot,ancestor,outcome,language ORDER BY n;
+CREATE TABLE soundlaws SELECT slid,slot,protolg,ancestor,outcome,language,'' AS context,count(*) AS n FROM soundlawsupport group by slot,ancestor,outcome,language ORDER BY n;
 ALTER TABLE soundlaws ADD id INT NOT NULL AUTO_INCREMENT FIRST, ADD PRIMARY KEY (id);
 DELETE FROM soundlaws WHERE n < 2;
 DELETE FROM soundlaws WHERE outcome = '' OR ancestor = '';
 
+CREATE INDEX idx_soundlaws_id ON soundlaws (id);
+CREATE INDEX idx_soundlaws_corrid ON soundlaws (corrid);
+CREATE INDEX idx_soundlaws_slot ON soundlaws (slot);
+CREATE INDEX idx_soundlaws_protolg ON soundlaws (protolg);
+CREATE INDEX idx_soundlaws_ancestor ON soundlaws (ancestor);
+CREATE INDEX idx_soundlaws_outcome ON soundlaws (outcome);
+CREATE INDEX idx_soundlaws_language ON soundlaws (language);
+CREATE INDEX idx_soundlaws_n ON soundlaws (n);
+CREATE INDEX idx_soundlaws_srcabbr ON soundlaws (srcabbr);
+CREATE INDEX idx_soundlaws_srcid ON soundlaws (srcid);
+
+CREATE INDEX idx_soundlawsupport_id ON soundlawsupport (id);
+CREATE INDEX idx_soundlawsupport_slid ON soundlawsupport (slid);
+CREATE INDEX idx_soundlawsupport_slot ON soundlawsupport (slot);
+CREATE INDEX idx_soundlawsupport_protolg ON soundlawsupport (protolg);
+CREATE INDEX idx_soundlawsupport_ancestor ON soundlawsupport (ancestor);
+CREATE INDEX idx_soundlawsupport_outcome ON soundlawsupport (outcome);
+CREATE INDEX idx_soundlawsupport_language ON soundlawsupport (language);
+CREATE INDEX idx_soundlawsupport_n ON soundlawsupport (n);
+
 UPDATE soundlawsupport l SET slid = (SELECT s.id FROM soundlaws s WHERE s.slot=l.slot AND s.ancestor=l.ancestor AND s.outcome=l.outcome AND s.language=l.language);
+
