962b444357bc6f2675c7f1043b9da19e64172370
stedtnik <stedt2ec48@berkeley.fake>
2012-04-17 22:44:51 +0000

    r482: update script : mkIndexSoundlaws.sql now incorporated into mkSoundlaws.sql

--- rootcanal/sql/mkm.sh
+++ rootcanal/sql/mkm.sh
@@ -5,6 +5,7 @@
 # jbl 3/7/2012, and earlier
 #
 # clean up any old files
+echo "NB: Syllabificationstation.pm and STEDTUtil.pm must be copied here from .../printutils!"
 rm lexicon.csv temp1.csv morphemes.txt 
 # extract lexicon records
 mysql -D stedt --default-character-set=utf8 -u $1 -p$2 -e "SELECT rn,reflex,gloss,gfn,gloss as glosshandle,language,grp,grpno,languagenames.srcabbr,lexicon.srcid,semkey,lexicon.lgid FROM lexicon,languagenames,languagegroups WHERE lexicon.lgid=languagenames.lgid AND languagenames.grpid=languagegroups.grpid;" > lexicon.csv
@@ -18,4 +19,3 @@ mysqlimport --local --default-character-set=utf8 -u $1 -p$2 stedt  morphemes.txt
 mysql --local stedt --default-character-set=utf8 -u $1 -p$2 < mkindexMorphemes.sql
 # extract "sound laws" from morphemes
 mysql --local stedt --default-character-set=utf8 -u $1 -p$2 < mkSoundLaws.sql
-mysql --local stedt --default-character-set=utf8 -u $1 -p$2 < mkindexSoundLaws.sql
