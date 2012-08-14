b9550c81a9df906821fe4059ed7f2d96689e1d00
stedtnik <stedt2ec48@berkeley.fake>
2012-08-14 00:16:55 +0000

    r554: Initial checkin of a experimental database interface implementing some UI conveniences, including but not limited to:
    - autosuggest
    - tabbed panes (for persistence)
    
    Not everything works yet, and the autosuggest javascript used has a
    few idiosyncrasies (features) which might even be called bugs.
    
    From a development point of view, the implementation is pretty simple:
    
    - only 23 lines of user-written javascript
    - a single css file, 75 lines
    - 7 php modules, totaling 262 lines
    - uses jQuery for tabs, and the autosuggest is also jQuery-based
    
    To deploy:
    
    I think all one needs to do is ensure that PHP is installed and put all the files in
    a webserver accessible directory (making sure they are executable).

---
+++ singapore/php/head.php
@@ -0,0 +1,15 @@
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>STEDT Search</title>
+  <style type="text/css">
+  /*<![CDATA[*/
+    @import "css/autoSuggest.css";
+    @import "css/smoothness/jquery-ui-1.8.22.custom.css";
+    @import "css/nocols.css";
+  /*]]>*/
+  </style>
+  <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui-1.8.22.custom.min.js"></script>
+  <script src="js/jquery.autoSuggest.js" type="text/javascript"></script>
+  <script src="js/stedtsearch.js" type="text/javascript"></script>
+</head>
