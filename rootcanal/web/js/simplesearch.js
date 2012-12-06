a38647a32c88b5d0fe389d4b8625395d37e76325
Daniel Bruhn <dwbruhn@users.noreply.github.com>
2012-12-06 00:43:30 +0000

    r675: Fixed allofam symbol display in mesoroot list (etymon view). Simple search no longer breaks if there's an extraneous slash at the end of the url. Renamed id's of sss gloss inputs to be clearer. Added tooltips to splash page. Added an extra example on the splash page. Search terms from splash page now persist when user is redirected to the tagging page (including non-auto-suggested entries in the lg field).

--- rootcanal/web/js/simplesearch.js
+++ rootcanal/web/js/simplesearch.js
@@ -51,7 +51,7 @@ function stedt_simplesearch_init() {
 		var tbl = e.findElement().id.sub('_search', '');
 		var params = {
 			tbl : tbl,
-			s : $F(tbl + '_searchinput'),
+			s : $F(tbl + '_searchgloss'),
 			f : $F(tbl + '_searchform')
 		};
 		if ($(tbl + '_searchlggrp'))
@@ -81,14 +81,47 @@ function stedt_simplesearch_init() {
 		onCreate: function() { $('spinner').show() },
 		onComplete: function() { if (0 == Ajax.activeRequestCount) $('spinner' ).hide() }
 	});
+
+	// persist search terms in language field from splash page
+	// fill in gloss values
+	if(document.URL.toQueryParams().t) {
+		$('lexicon_searchgloss').value = document.URL.toQueryParams().t;
+	}
+	// fill in auto-suggested lg values
+	var pItems = [];	// array to hold preFill objects for autosuggest setup below
+	// get auto-suggested lg param from URL, remove final comma, replace + sign with space, and split multiple lgs selected
+	if(document.URL.toQueryParams()['as_values_lg-auto']) {
+		// set up 'values' versions (remove final commas, change pluses to spaces, and split by commas)
+		var splash_lgs_v = document.URL.toQueryParams()['as_values_lg-auto'].replace(/,$/,'').replace(/\+/g,' ').split(',');
+		var splash_lgs_s = splash_lgs_v.invoke('replace',/^=/,''); // display versions (without initial = sign)
+		// create array of objects for autoSuggest preFill option
+		var lg_object;
+		var i;
+		for (i = 0; i < splash_lgs_v.length; i += 1) { // iterate through array of lg params and create object for each pair
+			lg_object = new Object();
+			lg_object.v = splash_lgs_v[i];
+			lg_object.s = splash_lgs_s[i];
+			pItems[i] = lg_object;
+		}
+	}
+
 	// there is also a hidden input[name=lg] in #etyma_search, so make sure we get the right one!
+	// note that for multiple preFill items, you have to pass in an array of objects
+	// see: https://drew.tenderapp.com/discussions/autosuggest/24-an-issue-with-prefill
 	jQuery('#lexicon_search input[name=lg]').autoSuggest(baseRef+'autosuggest/lgs',{
 		asHtmlID:"lg-auto",
 		startText:"",
 		selectedItemProp:"s",
 		selectedValuesProp:"v",
-		searchObjProps:"s"
+		searchObjProps:"s",
+		preFill: pItems
 	});
+	
+	// pre-fill language search box with any non-auto-suggested entry from the splash page
+	// replace pluses with spaces
+	if (document.URL.toQueryParams().lg) {
+		$('lg-auto').value=document.URL.toQueryParams().lg.replace(/\+/g,' ');
+	}
 };
 
 function vert_tog() {
