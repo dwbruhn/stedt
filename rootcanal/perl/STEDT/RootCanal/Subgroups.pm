d481c3c804e66b3e0509f923d5edd352c93b9b8e
Daniel Bruhn <dwbruhn@users.noreply.github.com>
2013-02-13 02:23:14 +0000

    r701: Support correct sorting of top-level group numbers greater than 9. Required change to languagegroups table in database (added 4 UNSIGNED TINYINT columns to explode grpno levels and extended length of grpno field to 15). Languagegroups editor now validates grpno before adding new entry. Prevent TableKit from messing with input fields ('noedit' class). To do: allow editing of current grpno's.

--- rootcanal/perl/STEDT/RootCanal/Subgroups.pm
+++ rootcanal/perl/STEDT/RootCanal/Subgroups.pm
@@ -10,7 +10,7 @@ sub view: StartRunmode {
 	my $a = $self->dbh->selectall_arrayref("SELECT grpid, grpno, grp, COUNT(lgid) AS num_lgs, plg, genetic
 		FROM languagegroups LEFT JOIN languagenames USING (grpid)
 		GROUP BY grpid
-		ORDER BY grpno");
+		ORDER BY grp0,grp1,grp2,grp3,grp4");
 	for my $row (@$a) {
 		my $str = $row->[1];
 		$str =~ s/\.0//g;
@@ -52,10 +52,23 @@ sub add : RunMode {
 	$self->require_privs(8);
 	my $q = $self->query;
 	if ($q->param("add_grpno") && $q->param("add_grp")) {
+
+		# get group number
+		my $grpno = $q->param("add_grpno");
+
+		# verify well-formedness of grpno: digits (first char can be 'X') separated by decimal points; required to end with digit
+		die "Group number must be a sequence of digits separated by periods.\n" unless $grpno =~ /^(X|\d+)(\.[\d]+)*$/;
+
+		# split group number by periods
+		my ($g0,$g1,$g2,$g3,$g4) = split /\./, $grpno;
+		if ($g0 eq 'X') {
+			$g0 = 255;	# support 'X' character as a top-level group by recording it as 255 in the table
+		}
+
 		my $sth = $self->dbh->prepare("INSERT languagegroups ("
-			. join(',', qw|grpno grp plg genetic|)
-			. ") VALUES (?, ?, ?, ?)");
-		eval { $sth->execute($q->param('add_grpno'), $q->param('add_grp'), $q->param('add_plg')||'', $q->param('add_genetic')||0)	};
+			. join(',', qw|grpno grp plg genetic grp0 grp1 grp2 grp3 grp4|)
+			. ") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		eval { $sth->execute($grpno, $q->param('add_grp'), $q->param('add_plg')||'', $q->param('add_genetic')||0, $g0||0, $g1||0, $g2||0, $g3||0, $g4||0) };
 		if ($@) {
 			my $err = "Couldn't create new languagegroup: $@";
 			die $err;
