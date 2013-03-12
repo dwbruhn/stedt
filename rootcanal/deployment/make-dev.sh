92f27c35a2353e73dc57c9f210f3440cc146a1d0
stedtnik <stedt2ec48@berkeley.fake>
2013-03-12 22:29:09 +0000

    r714: Test environment now provides link to release environment.

--- rootcanal/deployment/make-dev.sh
+++ rootcanal/deployment/make-dev.sh
@@ -14,5 +14,10 @@ rsync -rti --exclude '.svn' --delete ~/svn-rootcanal/web/scriptaculous/ ~/public
 perl -pe  's{<\!-- svnversion -->}{"<p>svn revision [<span style=\"color:red;font-weight:bold;\">DEV</span>]: " . `svnversion ~/svn-rootcanal/` . "</p>"}e' ~/svn-rootcanal/web/admin.tt > ~/public_html/dev/admin.tt
 perl -pe  's{logged in as}{<span style=\"color:red;font-weight:bold;\">DEV environment:</span> logged in as}' ~/svn-rootcanal/web/header.tt > ~/public_html/dev/header.tt
 perl -pe 's{unshift \@INC.*$}{unshift \@INC, \"\.\./\.\./pm-dev\", \"\.\./\.\./lib\";}' ~/svn-rootcanal/rootcanal.pl > ~/public_html/dev/rootcanal.pl
+
+# make admin page provide a link to the release environment
+perl -pe 's{^.*switch to the test environment.*$}{\t<li><a href="[% self_base.replace("dev/","rootcanal.pl/admin") %]">switch to the release environment</a></li>}' ~/public_html/dev/admin.tt > ~/public_html/dev/admin2.tt
+mv ~/public_html/dev/admin2.tt ~/public_html/dev/admin.tt
+
 mv ~/public_html/dev/rootcanal.pl ~/public_html/dev/rootcanal-dev.pl
 chmod u+x ~/public_html/dev/rootcanal-dev.pl
