68f0e7b1252ec621d7a01cb1993dd5ae0a8fade1
stedtnik <stedt2ec48@berkeley.fake>
2013-09-10 19:20:04 +0000

    r781: Incorporation of next version of validateContribution() and enabling of error message display. Still needs some help!

--- rootcanal/perl/STEDT/RootCanal/Admin.pm
+++ rootcanal/perl/STEDT/RootCanal/Admin.pm
@@ -70,32 +70,71 @@ sub validateContribution {
   my %results;
   my $lines;
   my $show_stopper = 0;
+  my $header_length;
+  my $row_length;
+  my @header; 
+  my %headerindex;
   while ( <$fh> ) {
+    chomp;
     $lines++;
-    #check each line
+    # check header
+    if ($lines == 1) {
+      @header = split "\t";
+      for (my $i = 0; $i < scalar @header; $i++) {
+        if ($header[$i] !~ /(gloss|reflex|pos)/) {
+	  $show_stopper = 1;
+	  push(@messages, $header[$i]. ' unexpected header column found.');
+	}
+	else {
+	  push(@messages, $header[$i]. " header column found: $i");
+	}
+        $headerindex{$header[$i]} = $i;
+      }
+    }
+    # So in the case of the test files, $headerindex{'gloss'} is 1.
+    # Now you can test the columns in the rest of the file:
+    
+    # check for missing values
+    my @columns = split "\t";
+    for (my $i = 0; $i < scalar @header; $i++) {
+      my $column = $columns[$i];
+      if ($i == $headerindex{'gloss'}) {
+        #print "@columns[$i]\n";
+	# do gloss tests 
+		# check well-formedness of gloss --- right now, checks for non-word characters in gloss; perhaps can be refined later
+        if ($columns[$i] =~ /[^\w\s]/) {
+          push(@messages, "non- character in column 'gloss', line $lines");
+          $show_stopper = 1 ;
+          }
+	# check if gloss exists
+	if ($column eq '') {
+	  push(@messages, 'no gloss.');
+	  $show_stopper = 1 ;
+	}
+      }
+      if ($i == $headerindex{'reflex'}) {
+	# do reflex tests
+	# check if reflex exists
+	    if ($column eq '') {
+	      push(@messages, 'no reflex.');
+	      $show_stopper = 1 ;
+	}
+        if ($columns[$i] =~ /[,'";.?]/) {
+          push(@messages, "unusual entry in column 'reflex', line $lines");
+          $show_stopper = 1;
+        }
+      }
+      if ($i == $headerindex{'pos'}) {
+	# do pos tests
+	# it is OK if pos field is empty!
+      }
+      # check POS if present
+    }
   }
-  push(@messages, $lines . ' lines read');
+  push(@messages, $lines . ' lines read, including header');
   $results{'status'}   = $show_stopper ? "Sorry, your file doesn't meet standards." : "File content OK!";
   $results{'messages'} = \@messages;
   seek($fh,0,0);
-  return %results;
-}
-
-sub validateMetadata {
-  my (%metadata) = @_;
-  my @messages;
-  my %results;
-  my $lines;
-  my $show_stopper = 0;
-  foreach my $key (keys %metadata) {
-    #check metadata elements
-    print STDERR "$key:  $metadata{$key}\n";
-    #$show_stopper = 1;
-    $lines++;
-  }
-  push(@messages, $lines . ' parameters seen');
-  $results{'status'}   = $show_stopper ? "Sorry, metadata insufficient." : "Metadata OK!";
-  $results{'messages'} = \@messages;
   return %results;
 }
 
