070d7fe7bc23b4965ba6889ee2e5fc52058c463c
Daniel Bruhn <dwbruhn@users.noreply.github.com>
2013-11-19 21:13:43 +0000

    r808: reverted changes from r805

--- rootcanal/perl/STEDT/RootCanal/Admin.pm
+++ rootcanal/perl/STEDT/RootCanal/Admin.pm
@@ -40,10 +40,9 @@ sub updatesequence : Runmode {
 }
 
 sub uploadFile {
-  my ($file,$fh) = @_;
-  my $upload_dir = '/tmp';
+  my ($file,$fh,$upload_dir) = @_;
 
-  open (UPLOADFILE, ">$upload_dir/$file" ) or die "$!";
+  open (UPLOADFILE, ">$upload_dir/$file") or die "$!";
   binmode UPLOADFILE;
   
   while ( <$fh> )
@@ -54,28 +53,13 @@ sub uploadFile {
   seek($fh,0,0);
 }
 
-
-sub loaddbna {
-  my (%metadata,$file) = @_;
-  my $upload_dir = '/tmp';
-
-  open (INPUTFILE, "<:encoding(UTF-8)", "$upload_dir/$file" ) or die "$!";
-  binmode INPUTFILE;
-  
-  while ( <INPUTFILE> )
-    {
-      # read line, insert into lexicon
-    } 
-  close INPUTFILE;
-}
-
 sub getheader {
       $_ = shift;
       @header = split "\t";
       for (my $i = 0; $i < scalar @header; $i++) {
-        if ($header[$i] !~ /(gloss|reflex|pos)/) {
-	  $show_stopper = 1;
-	  push(@messages, $header[$i]. ' unexpected header column found.');
+        if ($header[$i] !~ /\b(gloss|reflex|pos|id)\b/) {
+	  # $show_stopper = 1;
+	  push(@messages, $header[$i]. ': this header value is not used; column will be ignored.');
 	}
 	else {
 	  push(@messages, $header[$i]. " header column found: $i");
@@ -85,13 +69,20 @@ sub getheader {
 }
 
 sub validateMetadata {
-  my $fh = shift;
+  my $metaref = shift;
+  my %metadata = %{$metaref};
+  my @metatadatafields = shift;
+  foreach my $f (keys %metadata) {
+	print STDERR "$f:  $metadata{$f}\n";
+  }
+  my @m;
   my %results;
   my @messages;
     push(@messages, ' everything is fine for now');
-  $show_stopper = 1;
-  $results{'status'}   = $show_stopper ? "Your metadata is no good." : "Metadata OK!";
+  my $show_stopper = 1;
+  $results{'status'}   = $show_stopper ? "Metadata OK!" : "Sorry, your metadata has some problems." ;
   $results{'messages'} = \@messages;
+  $results{'metadata'} = \@m;
   return %results;
   }
 
@@ -101,6 +92,7 @@ sub validateContribution {
   my $lines;
   my $header_length;
   my $row_length;
+  my $problems = 0;
   while ( <$fh> ) {
     chomp;
     $lines++;
@@ -118,15 +110,17 @@ sub validateContribution {
       if ($i == $headerindex{'gloss'}) {
         #print "@columns[$i]\n";
 	# do gloss tests 
-		# check well-formedness of gloss --- right now, checks for non-word characters in gloss; perhaps can be refined later
-        if ($columns[$i] =~ /[^\w\s;\,\(\)\.\'\"\/\-]/) {
+	# check well-formedness of gloss --- right now, checks for non-word characters in gloss; perhaps can be refined later
+        if ($columns[$i] =~ /[^\w\s;\,\(\)\.\'\"\/\-\!\:]/) {
           push(@messages, "unusual character(s) in column 'gloss' <i>$columns[$i]</i>, line $lines");
           $show_stopper = 1 ;
+	  $problems++;
           }
 	# check if gloss exists
 	if ($column eq '') {
 	  push(@messages, 'no gloss.');
 	  $show_stopper = 1 ;
+	  $problems++;
 	}
       }
       if ($i == $headerindex{'reflex'}) {
@@ -135,20 +129,25 @@ sub validateContribution {
 	    if ($column eq '') {
 	      push(@messages, 'no reflex.');
 	      $show_stopper = 1 ;
+	      $problems++;
 	}
-        if ($columns[$i] =~ /['";.?]/) {
+        if ($columns[$i] =~ /[";.\?]/) {
           push(@messages, "unusual characters in column 'reflex' ($columns[$i]), line $lines");
           $show_stopper = 1;
+	  $problems++;
         }
       }
       if ($i == $headerindex{'pos'}) {
-	# do pos tests
+	# do part-of-speech tests
 	# it is OK if pos field is empty!
       }
-      # check POS if present
+      if ($i == $headerindex{'id'}) {
+	# check ID
+	# it is OK if ID field is empty!
+      }
     }
   }
-  push(@messages, $lines . ' lines read, including header');
+  push(@messages, $lines . ' lines read, including header. ' . $problems . ' problems identified.');
   $results{'status'}   = $show_stopper ? "Sorry, your file doesn't meet standards." : "File content OK!";
   $results{'messages'} = \@messages;
   seek($fh,0,0);
@@ -156,11 +155,9 @@ sub validateContribution {
 }
 
 sub load2db {
-#  my (%metadata) = shift;
-  my ($file,$dbh) = @_;
-  my $upload_dir = '/tmp';
+  my ($file,$dbh,$upload_dir,@m) = @_;
   
-  print STDERR 'file', $file;
+  #print STDERR 'file', $file;
 
   open (INPUTFILE, "<:encoding(UTF-8)", "$upload_dir/$file" ) or die "$!";
   binmode INPUTFILE;
@@ -170,28 +167,48 @@ sub load2db {
   my $header_length;
   my $row_length;
   my @messages;
+  # create new language names and source bib records
+  my $lgsort = $m[0];
+  $lgsort =~ tr/a-z/A-Z/;
+  $lgsort =~ s/ //g;
+  my $srcabbrExists = $dbh->selectrow_array("SELECT COUNT(*) FROM srcbib WHERE srcabbr=?", undef, $m[2]);
+  if ($srcabbrExists) {
+    push(@messages, '<span style="color:red">The srcabbr "' . $m[2] . '" already exists! Undo if this is not what you want!</span>');
+  }
+  else {
+    $dbh->do("INSERT srcbib (srcabbr, author, year, title, citation) values (?,?,?,?,?)", undef, $m[2],$m[3],$m[4],$m[5],$m[6]);
+  }
+  # find the lgcode value, if one exists. if not, make one up.
+  my $lgcode;
+  $lgcode= $dbh->selectrow_array("SELECT lgcode FROM languagenames WHERE lgsort=?", undef, $lgsort);
+  unless ($lgcode) {
+     $lgcode = $dbh->selectrow_array("select max(lgcode) from languagenames");
+     $lgcode += 1;
+  }
+  print STDERR 'lgcode: ' . $lgcode . ' lgsort: ' . $lgsort . ' grpid: ' . $m[9];
+  $dbh->do("INSERT languagenames (language, lgabbr, lgsort, srcabbr, lgcode, grpid) values (?,?,?,?,?,?)", undef, $m[0],$m[1],$lgsort,$m[2],$lgcode, $m[9]);
+  my $lgid = $dbh->selectrow_array("SELECT LAST_INSERT_ID();");
   while (<INPUTFILE> ) {
     chomp;
     $lines++;
     # check header
     if ($lines == 1) {
       getheader($_);
+      next;
     }
+    #print STDERR  $lines . ' lines read.';
     my @columns = split "\t";
     my $gloss  = @columns[ $headerindex{'gloss'} ];
     my $reflex = @columns[ $headerindex{'reflex'} ];
-    my $pos    = @columns[ $headerindex{'pos'} ];
+    my $srcid  = @columns[ $headerindex{'id'} ];
+    my $pos    = $headerindex{'pos'} ? @columns[ $headerindex{'pos'} ] : '';
     my $semkey = '';
-    my $lgid = 3000;
-    $dbh->do("INSERT lexicon (reflex, gloss, gfn, lgid, semkey) values (?,?,?,?,?)", undef, $reflex,$gloss,$pos,$lgid,$semkey);
-
-
-    my @columns = split "\t";
-#    push(@columns, $metadata{'language'}, $metadata{'source'});
-
+    $dbh->do("INSERT lexicon (reflex, gloss, gfn, lgid, semkey, srcid) values (?,?,?,?,?,?)", undef, $reflex,$gloss,$pos,$lgid,$semkey,$srcid);
   }
   push(@messages, $lines-1 . ' lines loaded');
-  $results{'status'} = "file loaded.";
+  #print STDERR  $lines-1 . ' lines loaded';
+  $results{'status'} = "name of file is: $file";
+  $results{'language id'} = $lgid;
   $results{'messages'} = \@messages;
   return %results;
 }
@@ -205,15 +222,29 @@ sub contribution : Runmode {
   my $self = shift;
   $self->require_privs(8);
   my $step = $self->query->param('step');
+  my $filename = $self->query->param('filename');
   my $file = $self->query->param('contribution');
+  my $lgid;
+  my $upload_dir = '/tmp';
+  my $metadatafields = ['language', 'lgabbr', 'srcabbr', 'author', 'year', 'title', 'citation', 'contributor', 'email'];
   my %metadata;
   my %results;
   my @validation;
-  if ($step eq 'upload') {
+  if ($step eq 'thanks') {
+    my $srcabbr = $self->query->param('srcabbr');
+    my $lgid = $self->query->param('lgid');
+    # ...but no thanks; if we are here, user must want to delete their data..
+    $self->dbh->do("DELETE FROM lexicon WHERE lgid=?", undef, $lgid);
+    $self->dbh->do("DELETE FROM srcbib WHERE srcabbr=?", undef, $srcabbr);
+    $self->dbh->do("DELETE FROM languagenames WHERE lgabbr=?", undef, $lgid);
+    $step = 'upload';
+  }
+  elsif ($step eq 'upload') {
     if ($file) {
       # upload file
       my $fh = $self->query->upload('contribution');
-      uploadFile($file,$fh);
+      $filename = $file;
+      uploadFile($file,$fh,$upload_dir);
       # validate it
       %results = validateContribution($fh);
       @validation = @{$results{'messages'}};
@@ -238,8 +269,20 @@ sub contribution : Runmode {
       next if $element =~ /^(btn|step)$/; # skip these
       $metadata{$element} = $self->query->param($element) if $self->query->param($element);
     }
+    my @m;
+    foreach my $f (@$metadatafields) {
+      if ($metadata{$f}) {
+	push @m,$metadata{$f};
+	print STDERR "$f:  $metadata{$f}\n";
+      }
+      else {
+	push @m,'';
+	print STDERR "$f:  empty\n";
+      }
+    }
+    push @m,$metadata{'grpid'};
     # check metadata
-    %results = validateMetadata(%metadata);
+    %results = validateMetadata(\%metadata,$metadatafields);
     @validation = @{$results{'messages'}};
     if ($results{'status'} =~ /Sorry/) {
       # oops try again!
@@ -247,7 +290,8 @@ sub contribution : Runmode {
     }
     else {
       # load to database
-      %results = load2db($file,$self->dbh);
+      %results = load2db($filename,$self->dbh,$upload_dir,@m);
+      $lgid = $results{'language id'};
       @validation = @{$results{'messages'}};
       $step = 'thanks';
     }
@@ -256,23 +300,38 @@ sub contribution : Runmode {
     # $step not set, must be first pass through: set current step to upload.
     $step = 'upload';
   }
-  
 
   foreach my $v (@validation) {
-    print STDERR "v:  $v\n";
+    print STDERR "contribution INFO:  $v\n";
   }
+
+ my $grpids = $self->dbh->selectall_arrayref("SELECT grpid,grpno,grp FROM languagegroups ORDER BY grp0,grp1,grp2,grp3,grp4 LIMIT 200");
+
   return $self->tt_process("admin/contribution.tt", {
 		step=>$step,
-		file=>$file,
-		message=>$results{'status'},
+		filename=>$filename,
+		file=>$file,				     
+		lgid=>$lgid,
+		grpids=>$grpids,
+		messages=>$results{'status'},
 		provided=>\%metadata,
 		validation=>\@validation,
-		metadata=> ['language','language abbreviation','source','author','year','contributor','email']
+		metadata=> $metadatafields
 		});
 		
 }
 
-
+sub bulkapproval : Runmode {
+  my $self = shift;
+  $self->require_privs(8);
+  # takes a list of tags and "approves" all non-STEDT tags for the selected user.
+  # everything is done via AJAX in the template. Nothing else to do here!
+  my $users = $self->dbh->selectall_arrayref("SELECT distinct username,uid FROM lx_et_hash
+  	LEFT JOIN users USING (uid)
+  	WHERE uid!=8
+  	ORDER BY username LIMIT 500");
+  return $self->tt_process("admin/bulkapproval.tt", {users => $users });
+}
 
 
 sub changes : Runmode {
