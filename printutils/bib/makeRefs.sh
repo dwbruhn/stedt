338b7cbe23dcaafceebb2033591242a0c4672581
stedtnik <stedt2ec48@berkeley.fake>
2013-12-11 19:27:31 +0000

    r838: tweaks to 'biblographic subsystem' to make it easier to use. see README

--- printutils/bib/makeRefs.sh
+++ printutils/bib/makeRefs.sh
@@ -1,7 +1,8 @@
 mysql -D stedt -e "select * from srcbib" > srcbib.csv
 mysql -D stedt -e "select srcabbr from srcbib" > cites.csv
 perl makeBib.pl srcbib.csv > stedtreferences.bib
-xelatex stedt_test1.tex 
-bibtex stedt_test1.aux
-xelatex stedt_test1.tex 
-xelatex stedt_test1.tex 
+xelatex bibtest.tex 
+bibtex bibtest.aux
+xelatex bibtest.tex 
+xelatex bibtest.tex
+cp stedtreferences.bib ..
