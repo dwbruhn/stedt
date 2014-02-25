05e4746b21b06338224aa2887838efcda30f54dc
stedtnik <stedt2ec48@berkeley.fake>
2014-02-25 22:41:48 +0000

    r998: improvements to the whole shebang making stuff...

--- printutils/disExperiment1/shebang.sh
+++ printutils/disExperiment1/shebang.sh
@@ -3,18 +3,18 @@
 # this script makes the entire STEDT.
 # it takes a while to run (15-20 mins, depending)
 #
-./makeFascOnly.sh 1 x x
-./makeFascOnly.sh 2 x x
-./makeFascOnly.sh 3 x x
-./makeFascOnly.sh 4 x x
-./makeFascOnly.sh 5 x x
-./makeFascOnly.sh 6 x x
-./makeFascOnly.sh 7 x x
-./makeFascOnly.sh 8 x x
-./makeFascOnly.sh 9 x x
-./makeFascOnly.sh 10 x x
+./makeFascOnly.sh 1 x x &
+./makeFascOnly.sh 2 x x &
+./makeFascOnly.sh 3 x x &
+./makeFascOnly.sh 4 x x &
+./makeFascOnly.sh 5 x x &
+./makeFascOnly.sh 6 x x &
+./makeFascOnly.sh 7 x x &
+./makeFascOnly.sh 8 x x &
+./makeFascOnly.sh 9 x x &
+./makeFascOnly.sh 10 x x &
 cd tex
-perl -ne "print if/include\{\d+\-/" *master.tex | perl -pe s'/.*://' > includes.tex
+perl -ne "print if/include\{\d+\-/" *x-x-master.tex | perl -pe s'/.*://' > includes.tex
 sed -e '/% insert includes here/r./includes.tex' masterMaster.tex > masterTemp.tex 
-#xelatex masterTemp.tex > master.log &
+xelatex masterTemp.tex > master.log
 #cp masterTemp.pdf /home/stedt/public_html/dissemination
--- printutils/frontmatter/makeFrontMatter.sh
+++ printutils/frontmatter/makeFrontMatter.sh
@@ -1,5 +1,6 @@
 cp *.tex ../disExperiment1/tex
 cd ../disExperiment1/tex
 xelatex frontmatter$1.tex
+bibtex frontmatter$1.aux
 xelatex frontmatter$1.tex
 xelatex frontmatter$1.tex
