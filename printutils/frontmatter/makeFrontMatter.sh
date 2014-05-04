4ee1304c06bc47b24150d102ac38793d6b9729f1
stedtnik <stedt2ec48@berkeley.fake>
2014-05-04 06:39:41 +0000

    r1067: refactored the templates used for the master and frontmatter options.

--- printutils/frontmatter/makeFrontMatter.sh
+++ printutils/frontmatter/makeFrontMatter.sh
@@ -1,3 +1,8 @@
+cp masterTemplate.tex frontmatter$1.tex
+if [ "$1" = '1col' ] ; then
+perl -i -pe 's/\\documentclass\[11pt,/\% \\documentclass[11pt,/'  frontmatter$1.tex
+perl -i -pe 's/\% \\documentclass\[11pt\]/\\documentclass[11pt]/'  frontmatter$1.tex
+fi
 cp *.tex ../disExperiment1/tex
 cd ../disExperiment1/tex
 xelatex frontmatter$1.tex
