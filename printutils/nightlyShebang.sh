be8657ae868ac4794d27185e8ab5c8ba1625a02d
stedtnik <stedt2ec48@berkeley.fake>
2014-05-15 02:02:04 +0000

    r1129: nightlyShebang.sh was not compiling front matter. Now it is.

--- printutils/nightlyShebang.sh
+++ printutils/nightlyShebang.sh
@@ -10,7 +10,7 @@ rm ~/printutils/disExperiment1/tex/*
 cd ~/printutils/bib
 ./makeRefs.sh > ~/printutils/shebang.log 2>&1
 cd ~/printutils/frontmatter
-./makeFrontmatter 1col
+./makeFrontMatter.sh 1col
 cp ~/printutils/disExperiment1/tex/frontmatter.pdf ~stedt/public_html/dissemination/frontmatter.pdf
 #
 rm ~/printutils/disExperiment1/tex/*
