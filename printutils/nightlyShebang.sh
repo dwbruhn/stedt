cd1f0f80ed3fe63c671a9e749137d9d0d9ab6e0f
stedtnik <stedt2ec48@berkeley.fake>
2014-08-13 18:27:54 +0000

    r1333: refactored nightlyShebang.sh to just do the D-T PDF. the other stuff is now in extras.

--- printutils/nightlyShebang.sh
+++ printutils/nightlyShebang.sh
@@ -9,26 +9,9 @@ svn up
 rm ~/printutils/disExperiment1/tex/*
 cd ~/printutils/bib
 ./makeRefs.sh > ~/printutils/shebang.log 2>&1
-cd ~/printutils/frontmatter
-./makeFrontMatter.sh 1col
-cp ~/printutils/disExperiment1/tex/frontmatter1col.pdf ~stedt/public_html/dissemination/frontmatter1col$DATE.pdf
-#
-rm ~/printutils/disExperiment1/tex/*
-cd ~/printutils/bib
-./makeRefs.sh > ~/printutils/shebang.log 2>&1
 cd ~/printutils/disExperiment1
 time ./shebang.sh >> ~/printutils/shebang.log 2>&1
 cp ~/printutils/disExperiment1/tex/masterTemp.pdf ~stedt/public_html/dissemination/master$DATE.pdf
-# extract comparanda and footnotes
-cd ~/printutils/disExperiment1/footnotes
-time ./comp_extractor.sh >> ~/printutils/shebang.log 2>&1
-time ./fn_extractor.sh >> ~/printutils/shebang.log 2>&1
-cp ~/printutils/disExperiment1/footnotes/comp_wrapper.pdf ~stedt/public_html/dissemination/comparanda$DATE.pdf
-cp ~/printutils/disExperiment1/footnotes/fn_wrapper.pdf ~stedt/public_html/dissemination/footnotes$DATE.pdf
-# extract prettified protoforms
-cd ~/printutils/disExperiment1/protoforms
-time ./pforms_extractor.sh >> ~/printutils/shebang.log 2>&1
-cp ~/printutils/disExperiment1/protoforms/pforms_wrapper.pdf ~stedt/public_html/dissemination/protoforms$DATE.pdf
 # copy log
 cp ~/printutils/shebang.log ~stedt/public_html/dissemination/shebang$DATE.log
 date
