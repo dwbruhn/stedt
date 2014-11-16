392d33d471b43b819b2c7acef5821074e0d5570b
Dominic Yu <gobbledegook@users.noreply.github.com>
2014-11-15 16:41:32 -0800

    update deployment script to work with git repo; add deployment config file; make minify.pl more robust

--- rootcanal/deployment/README.md
+++ rootcanal/deployment/README.md
@@ -1,30 +1,43 @@
-To deploy the STEDT database interface:
+To deploy the STEDT database interface on a production server:
 
 * Install MySQL
 * Load a STEDT database dump.
 * Install this code (see below)
 * Resolve all PERL module dependencies
 
-Installing and Updating the database interface code:
+Installing the database interface code:
 
 ```
 git clone https://github.com/stedt-project/sss.git
 cd sss/rootcanal/deployment
-# assuming your target web directory is ~/Sites/rootcanal, on your Mac...
-./gitdeploy.sh ~/Sites/rootcanal
-cp ../rootcanal.* ~/Sites/rootcanal
-# need to configure the app just a little bit...
-vi ~/Sites/rootcanal.conf  # edit in your stedt database login/password
-vi ~/Sites/rootcanal.pl    # edit @INC ???
+cp deploy.cfg.example deploy.cfg
+vi deploy.cfg
 ```
 
-When you need to update the code:
+Edit the deploy.cfg file with your target web directory (e.g., ~/Sites/rootcanal),
+and the directory where your script will see the perl modules.
+To minify javascript files, set minify = 1 and install either the google
+closure compiler <https://developers.google.com/closure/compiler/>
+or the JavaScript::Packer perl module.
+Once you're set up, you can run the deploy script to install most of the files,
+then copy over and configure the main CGI script:
 
-* Test your changes on your local machine
-* Commit them
+```
+./make.sh
+cp ../rootcanal.pl your-web-directory-here
+cp ../rootcanal.conf wherever-you-like
+# need to configure the app just a little bit...
+vi wherever-you-like/rootcanal.conf      # edit in your stedt database login/password
+vi your-web-directory-here/rootcanal.pl  # edit to point to your .conf file; edit @INC to include your module directory if necessary
+```
+
+When you or others have made changes on the repository and you want to
+update the code on your server to match:
+
 * Sign in to your production server
 * Deploy the changes
 
 ```
-./gitdeploy.sh ~/Sites/rootcanal
+cd sss/rootcanal/deployment
+./make.sh;
 ```
--- rootcanal/deployment/make.sh
+++ rootcanal/deployment/make.sh
@@ -1,14 +1,38 @@
 #! /bin/bash
 # use -rtvni to do a dry run with itemized changes
 
-git pull -v
-rsync -rti --exclude '.git*' --exclude 'js' --exclude 'scriptaculous/' --exclude 'admin.tt' ../web/ $1
-rsync -rti --exclude '.git*' ../perl/STEDT/ $1/STEDT
-# add new files to js directory if necessary, but let minify do the replacing
-rsync -rti --exclude '.git*' --ignore-existing --delete ../web/js/ $1/js
-rsync -rti --exclude '.git*' --ignore-existing --delete ../web/scriptaculous/ $1/scriptaculous/
-find ../web/js/ -name "*.js" -exec perl minify.pl {} \;
-find ../web/scriptaculous/*/ -name "*.js" -exec perl minify.pl {} \;
+cfgfile=deploy.cfg
+if [ -r $cfgfile ]; then
+	while IFS='= ' read lhs rhs
+	do
+		if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
+			rhs="${rhs%%*( )}"   # Del trailing spaces
+			declare $lhs="$rhs"
+		fi
+	done < $cfgfile
+fi
 
-git log | grep commit | head -1 | cut -f2 -d" " > deployed.txt
-perl -pe  's{<\!-- svnversion -->}{"<p>" . `git log | grep commit | head -1` . "</p>"}e' ../web/admin.tt > $1/admin.tt
+if [ ! -d $webdir ]; then
+	echo "No valid web directory specified! (tried '$webdir')"
+	exit 1
+fi
+if [ ! -d $modulesdir ]; then
+	echo "No valid modules directory specified! (tried '$modulesdir')"
+	exit 1
+fi
+
+git pull -v
+rsync -rti --exclude 'js' --exclude 'scriptaculous/' --exclude 'admin.tt' ../web/ $webdir
+rsync -rti ../perl/STEDT/ $modulesdir/STEDT
+if [ $minify = 1 ]; then
+	# add new files to js directory if necessary, but let minify do the replacing
+	rsync -rti --ignore-existing --delete ../web/js/ $webdir/js
+	rsync -rti --ignore-existing --delete ../web/scriptaculous/ $webdir/scriptaculous
+	find ../web/js -name "*.js" -exec perl minify.pl {} \;
+	find ../web/scriptaculous/* -name "*.js" -exec perl minify.pl {} \;
+else
+	rsync -rti --delete ../web/js/ $webdir/js
+	rsync -rti --delete ../web/scriptaculous/ $webdir/scriptaculous
+fi
+
+perl -pe 's{<\!-- svnversion -->}{"<p>git revision: " . `git rev-parse --short HEAD` . "</p>"}e' ../web/admin.tt > $webdir/admin.tt
